c5ef7d6102ce145f61fb1cdc5924a645d12f8e49
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-21 22:56:06 +0100

    BethanyPieShop.InventroyManagement

---
+++ OOP/ConsoleBPS.InventoryManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleBPS.InventoryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
