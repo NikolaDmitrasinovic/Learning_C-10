a11659ab2ff99ac786d900a445ef8f322c706355
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-23 13:15:21 +0100

    4 creating the classes - adding the order class

---
+++ OOP/ConsoleBPS.InventoryManagement/Domain/OrderManagement/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBPS.InventoryManagement.Domain.OrderManagement
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderFulfilmentDate { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public bool Fulfilled { get; set; } = false;
+
+        public Order()
+        {
+            Id = new Random().Next(9999999);
+
+            int numberOfSeconds = new Random().Next(100);
+            OrderFulfilmentDate = DateTime.Now.AddSeconds(numberOfSeconds);
+
+            OrderItems = new List<OrderItem>();
+        }
+    }
+}
---
+++ OOP/ConsoleBPS.InventoryManagement/Domain/OrderManagement/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBPS.InventoryManagement.Domain.OrderManagement
+{
+    internal class OrderItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int AmountOrdered { get; set; }
+
+        public override string ToString()
+        {
+            return $"Product ID: {ProductId} - Name: {ProductName} - Amount ordered: {AmountOrdered}";
+        }
+    }
+}
