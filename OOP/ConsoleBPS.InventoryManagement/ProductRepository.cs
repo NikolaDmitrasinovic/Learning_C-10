404c6341781c2e188189ad30848b54d6fd1b889b
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-27 22:51:36 +0100

    wip

---
+++ OOP/ConsoleBPS.InventoryManagement/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleBPS.InventoryManagement.Domain.ProductManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBPS.InventoryManagement
+{
+    internal class ProductRepository
+    {
+        private string directory = @"E:";
+        private string productFileName = "products.txt";
+
+        private void CheckForExistingProductFile()
+        {
+            string path = $"{directory}{productFileName}";
+
+            bool exists = File.Exists(path);
+            if (!exists)
+            {
+                if (Directory.Exists(path))
+                    Directory.CreateDirectory(directory);
+
+                using FileStream fs = File.Create(path);
+            }
+        }
+
+        public List<Product> LoadProductsFromFile()
+        {
+
+        }
+    }
+}
