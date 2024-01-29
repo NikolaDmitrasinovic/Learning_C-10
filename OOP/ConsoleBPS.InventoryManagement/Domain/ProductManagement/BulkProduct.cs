259d0ad59ecdf2ec0c152a987c30ffc793d27701
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-29 10:44:06 +0100

    fix: tidy up

--- OOP/ConsoleBPS.InventoryManagement/Domain/ProductManagement/BulkProduct.cs
+++ OOP/ConsoleBPS.InventoryManagement/Domain/ProductManagement/BulkProduct.cs
@@ -1,25 +1,22 @@
 ﻿using ConsoleBPS.InventoryManagement.Domain.General;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleBPS.InventoryManagement.Domain.ProductManagement
 {
-    public class BulkProduct : BoxedProduct
+    public class BulkProduct(
+        int id,
+        string name,
+        string? description,
+        Price price,
+        UnitType unitType,
+        int maxAmountInStock)
+        : BoxedProduct(id, name, description, price, unitType, maxAmountInStock)
     {
         public DateTime ExpiryDateTime { get; set; }
         public string? StorageInstructions { get; set; }
 
-        public BulkProduct(int Id, string name, string? description, Price price, UnitType unitType, int maxAmountInStock) 
-            : base(Id, name, description, price, unitType, maxAmountInStock)
-        {
-        }
-
         public void UseFreshBoxedProduct(int items)
         {
-            UseBoxProduct(3);
+            UseBoxProduct(items);
         }
     }
 }
